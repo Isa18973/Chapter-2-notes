b8d2eeef99b9c2f55f01300adf48312a6d10d3e9
dear <dear@fresnoU.org>
2023-02-15 10:00:05 -0800

    Initial Commit

--- inputNotes.js
+++ inputNotes.js
@@ -1,5 +1,6 @@
 const readline = require('readline-sync');
 
+//1
 let answer = readline.question("Do you want to hear a joke\n Yes or NO:");
 if (answer == "Yes") {
     let answer = readline.question("What starts with E, ends with E, and has only 1 letter in it? ");
@@ -11,16 +12,17 @@ else {
     console.log ('ok');
 }
 
-let base = readline.question ("Type in any number");
-if (base % 2== 0){
+//2
+let number = readline.question ("Type in any number");
+if (number  % 2== 0){
         console.log(` ${base} is divisible by 2`);
-}if (base % 3 == 0) {
+}if (number  % 3 == 0) {
         console.log(`${base} is divisible by 3`);
-}if (base % 4 == 0) {
+}if (number % 4 == 0) {
              console.log(`${base} is divisible by 4`);
-}if (base % 5 == 0) {
+}if (number % 5 == 0) {
                 console.log(`${base} is divisible by 5`);
-}if (base % 6 == 0) {
+}if (number % 6 == 0) {
     console.log(`${base} is divisible by 6`);
 } else {
         console.log(`${base} is not divisible by 2, 3, 4, 5, 6`);
@@ -28,15 +30,24 @@ if (base % 2== 0){
     }
 
 
-//
-let secret = Number(readline.question("Give me a number 1-100"));
-if (secret < 20)(
-    console.log ("Too Low");
-)if (secret > 20)(
-    console.log (" Too high");
-)if (secret == 20)(
-    console.log("You Won");
-)
+//4
+let guess = Number(readline.question("Give me a number 1-100"));
+if (guess == 35){
+    console.log ("You won")
+} else if (guess < 35){
+    console.log ("Too small")
+}else if (guess >35){
+    conosle.log(":too big")
+}else {
+    console.log("invalid");
+}
 
-//
 
+//5
+let cost  = number (readline.question("Enter meal cost "));
+let tax = cost * .08;
+let newCost = cost + tax
+console.log (`Your cost id $newCost .toFixed (2`)
+
+//7
+let mealCost = Numebr (realine.question ())
