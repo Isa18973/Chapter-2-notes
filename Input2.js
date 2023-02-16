ab04dd1d20a9f2a69c716d22fc5c8b9c418174a1
dear <dear@fresnoU.org>
2023-02-16 09:51:37 -0800

    Initial Commit

---
+++ Input2.js
@@ -0,0 +1,18 @@
+//1
+const readLine = require('readline-sync');
+let num1 = Number(readLine.question("Enter first number: "));
+let num2 = Number(readLine.question("Enter first number: "));
+let sum = num1 + num2;
+console.log(`${num1} + ${num2} = ${sum}`);
+
+//2
+const readline = require('readline-sync');
+let num3 = readline.question("Enter a number");
+let num4 = readline.question("Enter a number");
+let sum = num3 - num4;
+console.log (sum);
+//3
+const readline = require('readline-sync');
+let answer5 = readline.question("Enter a number");
+let answer6 = readline.question("Enter another number");
+let sum
