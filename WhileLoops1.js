3d298a73f27148d89bab22661a93cde5fd3b3abd
dear <dear@fresnoU.org>
2023-02-17 10:27:19 -0800

    Initial Commit

---
+++ WhileLoops1.js
@@ -0,0 +1,31 @@
+const readLine = require('readline-sync');
+
+//let x = 0;
+//while (x < 26){
+    //console.log(x);
+   // x++;
+//}
+
+//
+//let x = 0;
+//while (x< 51){
+  //  console.log(x);
+    //x++;
+//}
+
+//let x = -10;
+//while (x<101){
+  //  console.log(x);
+    //x++;
+//}
+
+//let x = -50;
+//while (x<0) {
+   // console.log(x);
+   // x++;
+//}
+let x = 0;
+while (x<26){
+    console.log(x);
+    x+2;
+}
