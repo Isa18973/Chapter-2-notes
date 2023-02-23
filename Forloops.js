6cf5bb698b1604de23eb161a5918981e7d8141d1
dear <dear@fresnoU.org>
2023-02-23 10:28:42 -0800

    Initial Commit

---
+++ Forloops.js
@@ -0,0 +1,63 @@
+//For Loops
+//for (let i = 0; i<= 10; i++){
+  //  console.log ("Hi");
+//}
+//for (let i = 0; i <= 10; i++){
+//console.log(i)
+//}
+
+
+// for (let i = 100; i<12; i--){
+//     console.log(i)
+// }
+
+// for (let i = 100; i<= 300 ; i++){
+// //     if(i % 2 == 0){
+// //         console.log (i)
+// //     }
+// //     console.log(i)
+// // }
+
+// let sum = 0;
+// for (let i = 0; i<= 100; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+// let sum = 0;
+// for (let i = 0; i<= 100; i++){
+//     if(i % 2 == 0){
+//         sum += i;
+//     }
+// }
+// console.log(`sum: ${sum}`);
+
+// 1
+// for (let i = 0; i <= 150; i++){
+// console.log(i)
+// }
+
+// 2
+//  for (let i = 0; i <= 40; i++){
+//     console.log(i ** 2)
+// }
+
+3
+//   for (let i = 0; i <= 30; i++){
+//     console.log(i ** 3)
+// }
+
+4
+// for (let i = 0; i <= 700; i++){
+//     if(i % 5 == 0){
+//         console.log(i)
+//     }
+// }
+
+5
+// for (let i = 350; i <= 1000; i++){
+//     if(i % 10 == 0){
+//         console.log(i)
+//     }
+// }
+
